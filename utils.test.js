f467645e03e8ce93b20c6e36d194be3ee461672a
Hen Shoster <hen@cidersecurity.io>
2021-08-23 18:50:12 +0300

    test 2

--- utils.test.js
+++ utils.test.js
@@ -1,6 +1,10 @@
 const sum = require('./utils');
 
-test('adds 1 + 2 to equal 3', () => {
+test('test 1', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
+test('test 2', () => {
+    expect(sum(2, 2)).toBe(4);
+  });
+  
