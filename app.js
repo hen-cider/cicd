b6b9e8d77c5699a03a63ecb03ff3278fda63262b
Hen Shoster <hen@cidersecurity.io>
2021-08-23 16:48:02 +0300

    test

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const sum = require('./utils');
 const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!' + sum(1,2))
 })
 
 app.listen(port, () => {
