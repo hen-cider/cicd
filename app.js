e254ef9e8c6ba53baffc4ab041ae9baf4ce22923
Hen Shoster <hen@cidersecurity.io>
2021-09-23 17:04:42 +0300

    loop

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const express = require('express')
-const sum = require('./utils');
-const app = express()
-const port = process.env.PORT || 3000
+const express = require("express");
+const sum = require("./utils");
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!' + sum(2,7))
-})
+for (let index = 0; index < 20; index++) {
+  console.log(index);
+}
+
+app.get("/", (req, res) => {
+  res.send("Hello World!" + sum(2, 7));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
